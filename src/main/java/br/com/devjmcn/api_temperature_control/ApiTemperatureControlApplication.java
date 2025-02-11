4285687f648eba1d90bb06de6c4babfe860f9f52
João Mourato <joaomouratocn@gmail.com>
2025-02-11 09:28:32 -0300

    primeiro commit

---
+++ src/main/java/br/com/devjmcn/api_temperature_control/ApiTemperatureControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.devjmcn.api_temperature_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTemperatureControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTemperatureControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devjmcn/api_temperature_control/ApiTemperatureControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devjmcn.api_temperature_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTemperatureControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
